892d2f29d8e043b344848dbab0b3840e9fd23d14
tkyg <80114433+tkyg@users.noreply.github.com>
2023-03-24 15:40:11 -0400

    done

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 // add any needed imports here
 
 function Users() {
+  const users = useSelector((state) => state.users)
   return (
     <div>
       <ul>
         Users!
         {/* Write code here that displays the usernames of all users in the Redux store */}
         {/* In addition, display the total number of users curently in the store */}
+        {users.map((user) => (
+          <li key={user.id}>
+            <p>{user.username}</p>
+          </li>
+        ))}
       </ul>
+        <p>Total Users: {users.length}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import usersReducer from "./features/users/usersSlice";
 
 // add imports and code
+const store = createStore(usersReducer)
 
 ReactDOM.render(
   // add imports and code
-  <App />,
+  <Provider store = {store}>
+    <App />
+  </Provider>,
   // add imports and code
   document.getElementById("root")
 );
